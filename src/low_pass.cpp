4617cc8f1576910690d9ae350dd752aecfb32ac2
Áron Járó <aron.jaro@gmail.com>
2023-02-19 16:32:14 +0100

    renaming stuff

--- src/low_pass.cpp
+++ src/low_pass.cpp
@@ -6,15 +6,16 @@
 
 struct LowPass : Module {
 	enum ParamId {
-		CUTOFF_PARAM,
-		LFM_DEPTH_PARAM,
-		RESONANCE_PARAM,
-		RES_MOD_DEPTH_PARAM,
+		FREQUENCY_PARAM,
+		F_MOD_DEPTH_PARAM,
+		Q_PARAM,
+		Q_MOD_DEPTH_PARAM,
+		RESONATOR_MODE_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
-		LFM_INPUT,
-		RES_MOD_INPUT,
+		F_MOD_INPUT,
+		Q_MOD_INPUT,
 		VPOCT_INPUT,
 		PING_INPUT,
 		FILTER_INPUT,
@@ -25,6 +26,7 @@ struct LowPass : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
+		RESONATOR_MODE_LIGHT,
 		LIGHTS_LEN
 	};
 
@@ -32,46 +34,54 @@ struct LowPass : Module {
 	cs::TunedDecayEnvelope<float> ping_processor;
 	dsp::BooleanTrigger ping_trigger;
 
+	bool res_mode = false;
+
 	LowPass()
 	: filter(cs::LowPass<float>(48000))
 	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(CUTOFF_PARAM, std::log2(10), std::log2(21000), std::log2(dsp::FREQ_A4), "Frequency", "Hz", 2);
-		configParam(LFM_DEPTH_PARAM, -1.f, 1.f, 0.f, "LFM depth");
-		configParam(RESONANCE_PARAM, 0, 1, 0, "Resonance");
-		configParam(RES_MOD_DEPTH_PARAM, -1.f, 1.f, 0.f, "Res. mod. depth");
-		configInput(LFM_INPUT, "Linear FM");
+		configParam(FREQUENCY_PARAM, std::log2(10.f), std::log2(21000.f), std::log2(55.f), "Frequency", "Hz", 2);
+		configParam(F_MOD_DEPTH_PARAM, -1.f, 1.f, 0.f, "FM depth");
+		configParam(Q_PARAM, 0.f, 1.f, 0.f, "Q");
+		configParam(Q_MOD_DEPTH_PARAM, -1.f, 1.f, 0.f, "Q mod. depth");
+		configParam(RESONATOR_MODE_PARAM, 0.f, 1.f, 0.f, "Resonator mode on");
+		configInput(F_MOD_INPUT, "Linear FM");
 		configInput(VPOCT_INPUT, "V/Oct");
 		configInput(PING_INPUT, "Ping");
-		configInput(RES_MOD_INPUT, "Resonance mod.");
+		configInput(Q_MOD_INPUT, "Q mod.");
 		configInput(FILTER_INPUT, "Filter");
 		configOutput(FILTER_OUTPUT, "Filter");
 	}
 
 	void process(const ProcessArgs& args) override
 	{
-		float freq_knob = params[CUTOFF_PARAM].getValue();
+		float freq_knob = params[FREQUENCY_PARAM].getValue();
 		float vpoct = inputs[VPOCT_INPUT].getVoltage();
-		float linfm_depth = 24000.f*args.sampleTime*dsp::quintic(params[LFM_DEPTH_PARAM].getValue());
-		float linfm = args.sampleRate*0.1*inputs[LFM_INPUT].getVoltage();
-		float cutoff_param = dsp::approxExp2_taylor5(freq_knob + vpoct) + linfm_depth*linfm;
+		float freq_tuning = dsp::approxExp2_taylor5(freq_knob + vpoct);
+		float f_mod_depth = 24000.f * args.sampleTime * dsp::quintic(params[F_MOD_DEPTH_PARAM].getValue());
+		float f_mod = args.sampleRate * 0.1f * inputs[F_MOD_INPUT].getVoltage();
+		float cutoff_param = freq_tuning + f_mod_depth * f_mod;
 
-		float res_knob = dsp::quintic(params[RESONANCE_PARAM].getValue());
-		float res_mod_depth = dsp::cubic(params[RES_MOD_DEPTH_PARAM].getValue());
-		float res_mod = 0.1*inputs[RES_MOD_INPUT].getVoltage();
-		float reso_param = res_knob + res_mod_depth*res_mod;
+		float q_knob = dsp::quintic(params[Q_PARAM].getValue());
+		float q_mod_depth = dsp::cubic(params[Q_MOD_DEPTH_PARAM].getValue());
+		float q_mod = 0.1f * inputs[Q_MOD_INPUT].getVoltage();
+		float reso_param = q_knob + q_mod_depth * q_mod;
 
-		//reso_param = rescale(reso_param, 0, 1, 0.5, 1000);
-		reso_param *= cutoff_param;
-
+		if(res_mode){
+			reso_param *= 5.f * freq_tuning;
+		}
+		else{
+			reso_param = rescale(reso_param, 0.f, 1.f, 0.5f, 1000.f);
+		}
+		
 		filter.setParams(cutoff_param, reso_param);
 
 		ping_processor.setFrequency(cutoff_param);
-
 		float ping_level = inputs[PING_INPUT].getVoltage();
 		float triggered = ping_trigger.process(0.f < ping_level);
 		float pulse = ping_level * ping_processor.process(args.sampleTime, triggered ? 1.f : 0.f);
 		float in = inputs[FILTER_INPUT].getVoltage();
+		
 		outputs[FILTER_OUTPUT].setVoltage(filter.process(in + pulse));
 	}
 
@@ -79,26 +89,42 @@ struct LowPass : Module {
 	{
 		filter = cs::LowPass<float>(e.sampleRate);
 	}
+
+	void resModeToggle(void)
+	{
+		res_mode = !res_mode;
+		lights[RESONATOR_MODE_LIGHT].setBrightness(res_mode? 1.f : 0.f);
+	}
 };
 
+struct ResModeButton : VCVButton{
+	void onDragStart(const DragStartEvent& e) override
+	{
+		VCVButton::onDragStart(e);
+		((LowPass*)this->module)->resModeToggle();
+	}
+};
 
 struct LowPassWidget : ModuleWidget {
 	LowPassWidget(LowPass* module) {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/low_pass.svg")));
 
-		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 15.856)), module, LowPass::CUTOFF_PARAM));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 34.237)), module, LowPass::LFM_DEPTH_PARAM));
-		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 63.908)), module, LowPass::RESONANCE_PARAM));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 82.289)), module, LowPass::RES_MOD_DEPTH_PARAM));
+		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 15.856)), module, LowPass::FREQUENCY_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 34.237)), module, LowPass::F_MOD_DEPTH_PARAM));
+		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 63.908)), module, LowPass::Q_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 82.289)), module, LowPass::Q_MOD_DEPTH_PARAM));
+		addParam(createParamCentered<ResModeButton>(mm2px(Vec(15.24, 95.468)), module, LowPass::RESONATOR_MODE_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 34.237)), module, LowPass::LFM_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 82.289)), module, LowPass::RES_MOD_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 34.237)), module, LowPass::F_MOD_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 82.289)), module, LowPass::Q_MOD_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 106.757)), module, LowPass::VPOCT_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.873, 106.757)), module, LowPass::PING_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 118.019)), module, LowPass::FILTER_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.873, 118.019)), module, LowPass::FILTER_OUTPUT));
+
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.431, 95.468)), module, LowPass::RESONATOR_MODE_LIGHT));
 	}
 };
 
