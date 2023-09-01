f7efb36091e08186ba324bce7222931c71d86da8
Áron Járó <aron.jaro@gmail.com>
2023-09-01 18:50:39 +0200

    added highpass filter module

--- src/high_pass.cpp
+++ src/high_pass.cpp
@@ -4,7 +4,7 @@
 #include "components/tuned_envelope.hpp"
 
 
-struct LowPass : Module {
+struct HighPass : Module {
 	enum ParamId {
 		FREQUENCY_PARAM,
 		F_MOD_DEPTH_PARAM,
@@ -28,12 +28,12 @@ struct LowPass : Module {
 		LIGHTS_LEN
 	};
 
-	cs::LowPass<float> filter;
+	cs::HighPass<float> filter;
 	cs::TunedDecayEnvelope<float> ping_processor;
 	dsp::BooleanTrigger ping_trigger;
 
-	LowPass()
-	: filter(cs::LowPass<float>(48000))
+	HighPass() 
+	: filter(cs::HighPass<float>(48000))
 	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(FREQUENCY_PARAM, std::log2(10.f), std::log2(24000.f), std::log2(55.f), "Frequency", "Hz", 2);
@@ -48,7 +48,7 @@ struct LowPass : Module {
 		configOutput(FILTER_OUTPUT, "Filter");
 	}
 
-	void process(const ProcessArgs& args) override
+	void process(const ProcessArgs& args) override 
 	{
 		float freq_knob = params[FREQUENCY_PARAM].getValue();
 		float vpoct = inputs[VPOCT_INPUT].getVoltage();
@@ -62,7 +62,7 @@ struct LowPass : Module {
 		float q_mod = 0.1f * inputs[Q_MOD_INPUT].getVoltage();
 		float reso_param = q_knob + q_mod_depth * q_mod;
 
-		reso_param = rescale(reso_param, 0.f, 1.f, 0.5f, 100.f);
+		reso_param *= freq_tuning;
 		
 		filter.setParams(cutoff_param, reso_param);
 
@@ -75,31 +75,31 @@ struct LowPass : Module {
 		outputs[FILTER_OUTPUT].setVoltage(filter.process(in + pulse));
 	}
 
-	void onSampleRateChange(const SampleRateChangeEvent& e) override
+		void onSampleRateChange(const SampleRateChangeEvent& e) override
 	{
-		filter = cs::LowPass<float>(e.sampleRate);
+		filter = cs::HighPass<float>(e.sampleRate);
 	}
 };
 
-struct LowPassWidget : ModuleWidget {
-	LowPassWidget(LowPass* module) {
+struct HighPassWidget : ModuleWidget {
+	HighPassWidget(HighPass* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/low_pass.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/high_pass.svg")));
 
-		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 15.856)), module, LowPass::FREQUENCY_PARAM));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 34.237)), module, LowPass::F_MOD_DEPTH_PARAM));
-		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 63.908)), module, LowPass::Q_PARAM));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 82.289)), module, LowPass::Q_MOD_DEPTH_PARAM));
+		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 15.856)), module, HighPass::FREQUENCY_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 34.237)), module, HighPass::F_MOD_DEPTH_PARAM));
+		addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(15.24, 63.908)), module, HighPass::Q_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(20.873, 82.289)), module, HighPass::Q_MOD_DEPTH_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 34.237)), module, LowPass::F_MOD_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 82.289)), module, LowPass::Q_MOD_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 106.757)), module, LowPass::VPOCT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.873, 106.757)), module, LowPass::PING_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 118.019)), module, LowPass::FILTER_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 34.237)), module, HighPass::F_MOD_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 82.289)), module, HighPass::Q_MOD_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 106.757)), module, HighPass::VPOCT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.873, 106.757)), module, HighPass::PING_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.607, 118.019)), module, HighPass::FILTER_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.873, 118.019)), module, LowPass::FILTER_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20.873, 118.019)), module, HighPass::FILTER_OUTPUT));
 	}
 };
 
 
-Model* modelLowPass = createModel<LowPass, LowPassWidget>("LowPass");
+Model* modelHighPass = createModel<HighPass, HighPassWidget>("HighPass");
